2adb8a3bf4d630dcb409a1f8974185379a91dc8f
soma-hagihara <soma-hagihara@cybozu.co.jp>
2025-05-21 17:08:50 +0900

    定数と変数の記述を追加

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('テスト');
+console.log('テスト123');
+
+// 変数 変わる数 variable
+
+// 昔
+// var test = 123;
+// var test = 'テスト';
+
+// let test = 123;
+// test += 123;
+
+const test = 123; // constant 定数
+test += 123;
+
+console.log(test);

9141ab8b5e141d039131ec2792f3f892f01b98b0
soma-hagihara <soma-hagihara@cybozu.co.jp>
2025-05-22 16:01:12 +0900

    変数・定数に関する記述を追加

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ console.log('テスト123');
 // let test = 123;
 // test += 123;
 
-const test = 123; // constant 定数
+const test = 123; // constant 定数 数字
 test += 123;
+
+// 動的型付け言語 変数中身を自動的に判定
+// 静的型付け言語 Java Visual Basic
+// integer int string 
+
+// 英語、アンダーバー、$
 
 console.log(test);

ab98146bf9305daeb6456b0c9cccb4dde9418c4d
soma-hagihara <soma-hagihara@cybozu.co.jp>
2025-05-22 17:32:03 +0900

    演算子に関する記述を追加

--- main.js
+++ main.js
@@ -74,4 +74,11 @@ const member_3 = {
   }
 };
 
-console.log(member_3['1kumi']['本田']['hobby']);
+console.log(member_3['1kumi']['本田']['hobby']);　
+
+const test_1 = 4; 
+const test_2 = 3; 
+
+const test_3 = test_1 + test_2; 
+
+console.log(test_3);
